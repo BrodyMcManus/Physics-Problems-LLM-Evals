e2d9484df2cafbfbb5e877bd7c960adbe1e7b471
BrodyM-Desk <brody.m.mcmanus@gmail.com>
2024-12-18 17:13:37 -0500

    More testing with Inspect tutorials

--- Hello-inspect.py
+++ Hello-inspect.py
@@ -1,22 +1,65 @@
-from inspect_ai import Task, task
-from inspect_ai.dataset import example_dataset
-from inspect_ai.scorer import model_graded_fact
+from inspect_ai import Task, eval, task
+from inspect_ai.dataset import Sample, hf_dataset
+from inspect_ai.scorer import choice
 from inspect_ai.solver import (               
-  prompt_template, generate, self_critique   
+  multiple_choice, system_message  
 )                                             
 
-DEFAULT_PROMPT="{prompt}"
+SYSTEM_MESSAGE = """
+You are a computer security expert and you are tasked with
+providing concise response to the following questions.
+Please assume that the reader is also well versed in
+computer security and provide a short response in a few words.
+"""
+
+# @task
+# def theory_of_mind():
+#     return Task(
+#         dataset=example_dataset("theory_of_mind"),
+#         solver=[
+#           prompt_template(DEFAULT_PROMPT),
+#           generate(),
+#           self_critique()
+#         ],
+#         scorer=model_graded_fact()
+#     )
+# @task
+# def security_guide():
+#     return Task(
+#         dataset=example_dataset("security_guide"),
+#         solver=[system_message(SYSTEM_MESSAGE), generate()],
+#         scorer=model_graded_fact(),
+#     )
+
+
+SYSTEM_MESSAGE="""
+Choose the most plausible continuation for the story.
+"""
+def record_to_sample(record):
+    return Sample(
+        input=record["ctx"],
+        target=chr(ord("A") + int(record["label"])),
+        choices=record["endings"],
+        metadata=dict(
+            source_id=record["source_id"]
+        )
+    )
+    
 
 @task
-def theory_of_mind():
-    return Task(
-        dataset=example_dataset("theory_of_mind"),
-        solver=[
-          prompt_template(DEFAULT_PROMPT),
-          generate(),
-          self_critique()
-        ],
-        scorer=model_graded_fact()
+def hellaswag():
+    
+    # dataset
+    dataset = hf_dataset(
+        path="hellaswag",
+        split="validation",
+        sample_fields=record_to_sample,
+        trust=True
     )
 
+    return Task(
+        dataset=dataset,
+        solver=[system_message(SYSTEM_MESSAGE),multiple_choice()],
+        scorer=choice(),
+    )
 
